4c882b019b3446a03ed967456252f061223f6dd7
ManfredSteyer <manfred.steyer@gmx.net>
2017-07-17 13:40:06 +0200

    first commit

---
+++ src/app/entities/flight.ts
@@ -0,0 +1,6 @@
+export interface Flight {
+  id: number; // int + double
+  from: string;
+  to: string;
+  date: string; // ISO-String: 2017-12-24T17:00:00.000+01:00
+}
---
+++ src/app/flight-search/flight-search.component.ts
@@ -0,0 +1,51 @@
+import { Component } from '@angular/core';
+import { Flight } from '../entities/flight';
+import { Http, URLSearchParams, Headers } from '@angular/http';
+
+@Component({
+  selector: 'flight-search',
+  templateUrl: './flight-search.component.html'
+})
+export class FlightSearchComponent {
+
+  from: string;
+  to: string;
+  flights: Array<Flight> = [];
+  selectedFlight: Flight;
+
+  //private http: Http;
+  constructor(private http: Http) {
+    //this.http = http;
+  }
+
+  search(): void {
+
+    let url = 'http://www.angular.at/api/flight';
+
+    let search = new URLSearchParams();
+    search.set('from', this.from);
+    search.set('to', this.to);
+
+    let headers = new Headers();
+    headers.set('Accept', 'application/json');
+
+    this
+      .http
+      .get(url, { search, headers})
+      .subscribe(
+        (resp) => {
+          this.flights = resp.json();
+        },
+        (errResp) => {
+          console.error('Fehler beim Laden', errResp);
+        }
+      );
+
+
+
+  }
+
+  select(f: Flight): void {
+    this.selectedFlight = f;
+  }
+}
